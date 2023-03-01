1bdddf8f22bd08693518f3b713a2a6ba86c12dff
Myeong <sowkosowko@naver.com>
2023-03-01 17:00:57 +0900

    first

---
+++ src/main/java/bandw/project/bandw/BandWApplication.java
@@ -0,0 +1,13 @@
+package bandw.project.bandw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandWApplication.class, args);
+	}
+
+}
